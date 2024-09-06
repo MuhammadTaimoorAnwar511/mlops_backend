a4f1429085fed718c964ce0d2494334160d30864
MuhammadTaimoorAnwar511 <anwartaimoor511@gmail.com>
2024-09-06 05:30:06 -0700

    checking vercel deployment 16 start agin with changes

--- app.py
+++ app.py
@@ -1,23 +1,22 @@
-from flask import Flask, request, jsonify
+# app.py
+from flask import Flask, request, jsonify, send_from_directory
 import joblib
 import numpy as np
 import os
-from dotenv import load_dotenv
-
-# Load environment variables from .env file
-load_dotenv()
 
 app = Flask(__name__)
-
-# Load the model
-model = joblib.load('bitcoin_model.pkl')
+model = joblib.load("bitcoin_model.pkl")
 
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json()
     features = np.array(data['features']).reshape(1, -1)
-    prediction = model.predict(features)[0]
-    return jsonify({'prediction': prediction})
+    prediction = model.predict(features)
+    return jsonify({'prediction': prediction[0]})
+
+@app.route('/')
+def index():
+    return send_from_directory('.', 'template/index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- config.js
+++
@@ -1,3 +0,0 @@
-@app.route('/config')
-def config():
-    return jsonify({'apiUrl': os.getenv('API_URL', 'http://127.0.0.1:5000/predict')})
