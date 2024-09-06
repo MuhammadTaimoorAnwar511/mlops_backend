bf4402c890f19e80c2e9c4bfd4f2d4068d2315d7
MuhammadTaimoorAnwar511 <anwartaimoor511@gmail.com>
2024-09-06 03:12:46 -0700

    checking vercel deployment 11

--- model_train.py
+++ model_train.py
@@ -1,4 +1,3 @@
-# model_train.py
 import yfinance as yf
 import pandas as pd
 from sklearn.model_selection import train_test_split
@@ -7,7 +6,7 @@ import joblib
 
 # Fetch Bitcoin price data
 ticker = "BTC-USD"
-data = yf.download(ticker, start="2023-01-01", end="2023-07-01", interval="1d")
+data = yf.download(ticker, start="2023-05-01", end="2023-07-01", interval="1d")
 
 # Use only the 'Close' price for simplicity
 data = data[['Close']].copy()  # Make a copy to avoid SettingWithCopyWarning
@@ -24,5 +23,5 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 model = LinearRegression()
 model.fit(X_train, y_train)
 
-# Save model
-joblib.dump(model, "bitcoin_model.pkl")
+# Save model with compression
+joblib.dump(model, "bitcoin_model.pkl", compress=3)
